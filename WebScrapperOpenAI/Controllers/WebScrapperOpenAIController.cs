c4c91359d916fbbfefcf46676c0c9d25648d930b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-11 13:46:40 -0300

    WebScrapper created

---
+++ WebScrapperOpenAI/Controllers/WebScrapperOpenAIController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebScrapperOpenAI.Business;
+
+namespace WebScrapperOpenAI.Controllers
+{
+    [Route("api/[controller]/v1")]
+    [ApiController]
+    public class WebScrapperOpenAIController : ControllerBase
+    {
+
+        private readonly WebScrapperOpenAIBusiness _wsOpenAIBusiness;
+
+        public WebScrapperOpenAIController(WebScrapperOpenAIBusiness wsOpenAIBusiness)
+        {
+            _wsOpenAIBusiness = wsOpenAIBusiness;            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CertificationStudyPlan([FromBody] string certification)
+        {
+            var result = await _wsOpenAIBusiness.CertificationStudyPlan(certification);
+            return Ok(result);
+        }
+
+
+    }
+}
