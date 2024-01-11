ffae72bb82dd3e8736d2f10be5ac0e921b2436f3
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-11 17:41:21 -0300

    Created IWebScrapper

---
+++ WebScrapperOpenAI/Business/Interfaces/IWebScrapper.cs
@@ -0,0 +1,7 @@
+﻿namespace WebScrapperOpenAI.Business.Interfaces
+{
+    public interface IWebScrapper
+    {
+         Task<String> CertificationStudyPlan(string certification, string scheduledDay);
+    }
+}
